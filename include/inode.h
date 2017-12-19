43ecb868bbc544c9f7e3328c3e5e14070a6ce4df
buck5060 <buck5060@gmail.com>
2017-12-19 14:52:28 +0800

    [INODE] Create inode Header File

---
+++ include/inode.h
@@ -0,0 +1,63 @@
+#ifndef _INODE_H_
+#define _INODE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+#include "log.h"
+
+#define BYTE_SIZE 8
+#define INODE_SIZE 128
+#define INODE_NUM 4096
+
+typedef struct inode
+{
+	short id;
+	int filesize;
+	short uid;
+	short gid;
+	short filemode;
+	timestamp timestamp;
+	int num[15];
+	char filename[32];
+
+} inode;
+
+
+typedef struct superblock_2
+{
+	short block_used;
+	short block_unuse;
+	short inode_used;
+	short inode_unuse;
+	extern uint8_t inode_map[INODE_NUM/8];
+
+} superblock_2;
+
+typedef struct single_indirect_block
+{
+	short num12[512];
+
+} single_indirect_block;
+
+
+int get_inode_bypath (const char *path, inode *inode);
+
+int get_inode_byid (const int inode_id, inode *inode);
+
+int update_inode_bypath (const char *path, const inode *inode);
+
+int update_inode_byid (const int *inode_id, const inode *inode);
+
+int create_inode (const char *path, const inode *inode);
+
+int del_inode (const char *path);
+
+int del_inode (const int inode_id);
+
+
+#endif
+
